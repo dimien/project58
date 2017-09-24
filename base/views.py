74679533d6f60e533b74acd485d222d0b8739f74
d.malovaniy <d.malovaniy@modnakasta.ua>
2017-09-24 21:45:40 +0300

    render band's props

--- base/views.py
+++ base/views.py
@@ -2,14 +2,21 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import Band
 
 # Create your views here.
 
 def index(request):
-	bands = Band.objects.all()
-	print bands
-	# context = get_object_or_404(Band, pk=1)
-	context = {'bands': bands}
-	return render(request, 'base/index.html', context)
+    name = get_object_or_404(Band.objects.values_list("name", flat=True))
+    genere = get_object_or_404(Band.objects.values_list("genere", flat=True))
+    created = get_object_or_404(Band.objects.values_list("created", flat=True))
+    rip = get_object_or_404(Band.objects.values_list("rip", flat=True))
+    context = {
+        "band_name": name,
+        "band_genere": genere,
+        "band_created": created,
+        "band_rip": rip
+    }
+    return render(request, 'base/index.html', context)
+
